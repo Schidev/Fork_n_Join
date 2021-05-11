5363d6d1803e7aa2f029eb9c0f9187beea988692
Denis Schifrin <56796561+Schidev@users.noreply.github.com>
2021-05-11 17:15:47 +0300

    .

---
+++ MatrixTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fork_n_Join;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fork_n_Join.Tests
+{
+    [TestClass()]
+    public class MatrixTests
+    {
+        [TestMethod()]
+        public void SumOfElementTest()
+        {
+            int[][] matrix = new int[3][]{
+                                        new int[] { 1, 2, 3, 4, 5 },
+                                        new int[] { 6, 7, 8, 9, 10 },
+                                        new int[] { 11, 12, 13, 14, 15 } };
+
+            int expected = 0;
+            for (int m = 0; m < matrix.Length; m++)
+            {
+                for (int n = 0; n < matrix[m].Length; n++)
+                {
+                    expected += matrix[m][n];
+                }
+            }
+
+            int actual = Matrix.SumOfElement(matrix);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
