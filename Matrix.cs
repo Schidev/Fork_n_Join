f60f5de8340d3d6d4f5aefe6af0322097e5b1f99
Denis Schifrin <56796561+Schidev@users.noreply.github.com>
2021-05-11 17:12:17 +0300

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fork_n_Join
+{
+    public class Matrix
+    {
+        private static Barrier _barrier;
+        private static int[][] _matrix;
+        private static int[] _rowsum;
+
+        public static int SumOfElement(int[][] matrix)
+        {
+            _matrix = matrix;
+            _rowsum = new int[matrix.Length];
+            _barrier = new Barrier(matrix.Length + 1);
+            ThreadsControl();
+            
+            return Result();
+        }
+
+        private static void ThreadsControl()
+        {
+            for (int i = 0; i < _barrier.ParticipantCount - 1; i++)
+            {
+                new Thread(RowSum).Start(i);
+            }
+            _barrier.SignalAndWait();
+        }
+
+        private static void RowSum(object o)
+        {
+            int i = (int)o;
+
+            for (int j = 0; j < _matrix[i].Length; j++)
+            {
+                _rowsum[i] += _matrix[i][j];
+            }
+
+            _barrier.SignalAndWait();
+        }
+
+        private static int Result()
+        {
+            int result = 0;
+            for (int k = 0; k < _rowsum.Length; k++)
+            {
+                result += _rowsum[k];
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fork_n_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedMatrix = new int[3][]
+                                                { new int[] { 1,2, 3, 4, 5 },
+                                                  new int[] { 6, 7, 8, 9, 10 },
+                                                  new int[] { 11, 12, 13, 14, 15 } };
+
+            Console.WriteLine("Sum of all elements in this matrix is equal " + Matrix.SumOfElement(jaggedMatrix));
+
+            Console.ReadKey();
+        }
+    }
+}
